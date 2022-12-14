53276db5246906867c9fbfafd88fb8b15486ca17
tebibux <tebibusolomon79@gmail.com>
2022-12-14 07:15:40 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import formDiv from "./maker/boxMaker";
 
 // retrieving the container from the template.
 const container = document.querySelector('.container');
+
 // creating the form 
 const form = new Maker('form', 'form', '').domMaker();
-
+const messageBox = new Maker('div', 'messageBox').domMaker();
+form.appendChild(messageBox);
 // create form element 
 // create name element for name
 // label and input
@@ -47,10 +49,12 @@ password.classList.add('formDiv');
 form.appendChild(password);
 
 // formDiv('divClassName', 'divId', 'inputId', 'inputName', 'inputType', 'labelName');
-const password2 = formDiv('uPassword', '', 'userPass2', 'password2', 'password2', 'Confirm Password:');
+const password2 = formDiv('uPassword', '', 'userPass2', 'password2', 'password', 'Confirm Password:');
 password2.classList.add('formDiv');
 // append the email to the form
 form.appendChild(password2);
+
+
 
 const submitBtn = new Maker('button', 'button', 'submitBtn').domMaker();
 submitBtn.setAttribute('type', 'submit');
@@ -59,29 +63,57 @@ form.appendChild(submitBtn);
 // appending the form to the container
 container.appendChild(form);
 
-form.addEventListener('click', (e) => {
-	e.preventDefault();
-	validityChecker();
-});
+form.addEventListener('submit', (e) => {
+	validityChecker(e);
+})
 
 
-function validityChecker(){
+
+function validityChecker(event) {
+	let message = [];
 	const userName = document.getElementById('username');
-	const userEmailValue = document.getElementById('userEmail').value.trim();
-	const countryValue = document.getElementById('userCountry').value.trim();
-	const zipCodeValue = document.getElementById('userZipCode').value.trim();
-	const passwordValue = document.getElementById('userPass').value.trim();
-	const password2Value = document.getElementById('userPass2').value.trim();
+	const userEmailValue = document.getElementById('userEmail');
+	const passwordValue = document.getElementById('userPass');
+	const password2Value = document.getElementById('userPass2');
 
-	if(userName.value === ''){
-		// add error class
-		console.log('hi')
-		// popup message
-		userName.classList.add('error')
-		// urName.setCustomValidity('Please Inter your name');
-	}else{
-		console.log(userName.value.trim())
+	// check if the user name 
+	if (userName.value === '' || userName.value === null) {
+		message.push('pass word shouldn\'t be null or empty');
+		// userName.classList('error');
+		displayError(message, event)
 	}
+	else if (userName.value.length < 4) {
+		// if the user name length less than 4
+		message.push('user name should be greater than 4 character');
+		// userName.classList('error')
+		displayError(message, event)
+	}
+	// check if the email is valid
+	if (userEmailValue.value === '' || userEmailValue.value === null) {
+		message.push('email shouldn\'t be empty');
+		// userEmailValue.classList('error')
+		displayError(message, event);
+	}
+	if(passwordValue.value.length <= 4){
+		// password too short
+		message.push('Password too short');
+		displayError(message, event);
+	}
+	// check if the two password match
+	if (passwordValue.value !== password2Value.value) {
+		// password doesn't match
+		message.push('Password doesn\'t match');
+		// passwordValue.classList('error');
+		// password2Value.classList('error')
+		displayError(message, event);
+	}
+}
 
 
+function displayError(message, event) {
+	if (message.length > 0) {
+		event.preventDefault();
+		messageBox.style.backgroundColor = '#ff000069';
+		messageBox.innerText = message.join(', ')
+	}
 }
